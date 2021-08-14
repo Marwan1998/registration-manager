52730c5ea0640efb2331cbd8648292e763c4f3aa
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-08-15 00:08:15 +0200

    added addPage, modified other pages

---
+++ src/test.js
@@ -0,0 +1,8 @@
+
+const obj1 = {id: 1, name: 'SpiderMan', power: 6, of: 10};
+const obj2 = {id: 2, name: 'superman', power: 8, of: 10};
+const obj3 = {id: 3, name: 'houlk', power: 7, of: 10};
+const Name = 'MG45';
+
+console.log(`%c Hello ${Name}`, 'color: blue; font-size: 8rem;');
+console.table({obj1, obj2, obj3});
