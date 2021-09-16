e6a1cd1d45c2218ac39827cc8b6c81e93af77825
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-09-17 01:36:05 +0200

    changed the html files for server to serve static files ('public')

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const path = require('path')
 
 
 const app = express();
 
-app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("/public"));
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('logSignUp')
+
+app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/home', (req, res) => {
+    res.render('home')
 })
+
+app.get('/add', (req, res) => {
+    res.render('add')
+})
+
+app.get('/logsignup', (req, res) => {
+    res.render('logsignup')
+})
+
+app.get('/review', (req, res) => {
+    res.render('review')
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+
+
 
 
 app.listen(3000, () => console.log("Server started on port 3000"));
